27ae1c6a5ae4742ebccc7bb59bb70bd200cad79d
scammi <scammi@gmail.com>
2018-11-16 17:20:42 -0300

    qr code does not repeate

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
-var links;
 
-document.getElementById("boton").addEventListener("click", myFunction);
-function myFunction() {
-    //Initializes node
+
+document.getElementById("boton").addEventListener("click", medPass);
+
+function medPass() {
     const IPFS = require('ipfs')
 
     const options = {
-
      "API": {
         "HTTPHeaders": {
             "Access-Control-Allow-Origin": [
@@ -28,39 +27,35 @@ function myFunction() {
         }
      }
     }
-
     const node = new IPFS(options);
-
+    //starts
     node.on('ready', async () => {
     const version = await node.version()
-
     //Adds file to ipfs
     const filesAdded = await node.files.add({
         path: 'text.txt',
         content: Buffer.from(document.getElementById("textfield").value)
       })
      console.log('Added file:', filesAdded[0].path, filesAdded[0].hash)
-
     //Creates link to file added
      document.getElementById("link").innerHTML= "https://ipfs.io/ipfs/"+filesAdded[0].hash
-     links = document.getElementById("link").innerHTML
+     var links = document.getElementById("link").innerHTML
+     var qrlink = document.getElementById("downloadLink")
+     var qrdiv = document.getElementById("qrcode")
+      //Creates Qr
+      if (qrdiv.hasChildNodes()) {
+        qrdiv.innerHTML =''
+      }
+      new QRCode(qrdiv, links)
 
-    //
-    var qrlink = document.getElementById("downloadLink")
-    var qrdiv = document.getElementById("qrcode")
-    //Creates Qr
-    new QRCode(qrdiv, links)
 
-    setTimeout(makeLink, 1000)
+     setTimeout(makeDownloadLink, 1000)
+      });
 
-
-    });
-
-    function makeLink(){
+    function makeDownloadLink(){
       var qrlink = document.getElementById("downloadLink")
       var imgsrc = document.getElementsByTagName("img")[0].src
       qrlink.href = imgsrc
       qrlink.innerHTML = "Click here to downloar your QRcode"
-      console.log(imgsrc)
     }
 }
